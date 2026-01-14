28233921deaa92bfd1b4376aa792ae2a4225078a
Marie-Lorraine Collignon <marie-lorraine.collignon@fms-ea.com>
2026-01-14 14:04:08 +0100

    un autre message de commit

---
+++ Raccourcis/Raccourcis.js
@@ -0,0 +1,45 @@
+////////////////// DUPLICATE LINE /////////////
+// Shortcut: Alt + Shift + ↓
+
+const title = "VS Code";
+
+
+////////////////// MOVE LINE //////////////////
+// Shortcut: Alt + ↑ / Alt + ↓
+
+const subtitle = "Keyboard shortcuts";
+
+
+////////////////// SELECTION //////////////////
+// Shortcut: Ctrl + D
+
+let counter = 0;
+counter = counter + 1;
+counter = counter + 1;
+counter = counter + 1;
+
+
+////////////////// RENAME //////////////////
+// Shortcut: F2
+
+let userName2 = "MarieLorraine";
+console.log(userName2);
+console.log(userName2);
+
+
+////////////////// COMMENT //////////////////
+// Shortcut: Ctrl + /
+
+console.log("This line can be commented");
+
+
+////////////////// COMMAND PALETTE //////////////////
+// Shortcut: Ctrl + Shift + P
+// "Format Document"
+// "Rename Symbol"
+
+
+////////////////// GO TO FILE //////////////////
+// Shortcut: Ctrl + P
+
+
